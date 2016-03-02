b5be9d2d34dca09f67275c8507780fd0c3b63055
Steven Wade <steven@stevenwade.co.uk>
2016-03-02 20:06:37 +0000

    installing the slack notifier plugin.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# Install and configure the server.
 include_recipe 'gocd::server'
 
 # Install the nuget poller.
@@ -11,6 +12,12 @@ gocd_plugin 'go-nuget-poller' do
   plugin_uri 'https://github.com/ThoughtWorksInc/go-nuget-poller-plugin/releases/download/v1.0.2/go-nuget-poller-plugin.jar'
 end
 
+# Install the artifactory poller
 gocd_plugin 'artifactory-poller' do
   plugin_uri 'https://github.com/varchev/go-generic-artifactory-poller/releases/download/0.1.0/go-generic-artifactory-poller.jar'
+end
+
+# Install the slack notifier
+gocd_plugin 'slack-notifier' do
+  plugin_uri 'https://github.com/ashwanthkumar/gocd-slack-build-notifier/releases/download/v1.1.2/gocd-slack-notifier-1.1.2.jar'
 end
