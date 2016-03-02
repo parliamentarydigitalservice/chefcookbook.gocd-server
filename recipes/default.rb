57751a350b59781d2d67351775754ab334a7478e
Steven Wade <steven@stevenwade.co.uk>
2016-03-02 17:24:02 +0000

    installing the NuGet and Artifactory poller.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,12 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'gocd::server'
+
+# Install the nuget poller.
+gocd_plugin 'go-nuget-poller' do
+  plugin_uri 'https://github.com/ThoughtWorksInc/go-nuget-poller-plugin/releases/download/v1.0.2/go-nuget-poller-plugin.jar'
+end
+
+gocd_plugin 'artifactory-poller' do
+  plugin_uri 'https://github.com/varchev/go-generic-artifactory-poller/releases/download/0.1.0/go-generic-artifactory-poller.jar'
+end
