3f4efcd2ae8bb5f2ad6558ee1710b9efec9cf4bd
Steven Wade <steven@stevenwade.co.uk>
2016-03-14 16:04:41 +0000

    getting started with some Test Kitchen testing.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
-describe 'template::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+describe 'go server service' do
+  it 'should be running' do
+    expect(service('go-server')).to be_running
+    expect(service('go-server')).to be_enabled
+  end
+
+  it 'should be listening on port 8153' do
+    expect(port(8153)).to be_listening
+  end
+
+  it 'should be listening on port 8154' do
+    expect(port(8154)).to be_listening
   end
 end
