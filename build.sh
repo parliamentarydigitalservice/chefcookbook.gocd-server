af84c13956d87b4912507a5841f6c3b8cb1df3d6
James McKay <code@jamesmckay.net>
2016-12-14 15:04:14 +0000

    Move the AWS region into an environment variable.
    We're using us-east-1 for testing purposes here. To be switched to
    eu-west-1 when we are satisfied that everything is working correctly.

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+export AWS_REGION=us-east-1
+
 ## Execute the CI task from the Rakefile
 #sudo $(which chef) exec rake ci
 
 ## Obtain the source AMI to use for our Packer build.
-base_ami_id=$(aws ec2 describe-images --filters Name=tag-key,Values=name Name=tag-value,Values=linux-ubuntu-base --output text --query 'Images[*].[ImageId,CreationDate]' | sort -n -k 2 -r | cut -f1 | head -n1)
+base_ami_id=$(aws ec2 describe-images \
+    --filters Name=tag-key,Values=name Name=tag-value,Values=linux-ubuntu-base \
+    --output text --query 'Images[*].[ImageId,CreationDate]' \
+    --region $AWS_REGION \
+    | sort -n -k 2 -r | cut -f1 | head -n1 \
+)
 
 ## Build an AMI for this cookbook
 $(which chef) exec rake packer[$base_ami_id]
