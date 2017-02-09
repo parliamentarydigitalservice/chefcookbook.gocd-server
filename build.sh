f36331cd18fd47c9503601e498a262c443e5bfed
James McKay <code@jamesmckay.net>
2017-02-09 16:11:48 +0000

    Use correct AMI and latest version of Go.CD cookbook

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ base_ami_id=$(aws ec2 describe-images \
     --filters Name=tag-key,Values=Name Name=tag-value,Values=base-ubuntu-16.04 \
     --output text --query 'Images[*].[ImageId,CreationDate]' \
     --region $AWS_REGION \
-    | sort -n -k 2 -r | cut -f1 | head -n1 \
+    | sort -k 2 -r | cut -f1 | head -n1 \
 )
 
 ## Build an AMI for this cookbook
